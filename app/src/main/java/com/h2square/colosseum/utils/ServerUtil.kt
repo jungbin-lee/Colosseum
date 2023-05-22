616148ffcc38ce94bf9e87e03ab959bf1b7b01ae
jungbin-lee <hihihi486@naver.com>
2023-05-22 18:57:11 +0900

    add Utils

--- app/src/main/java/com/h2square/colosseum/utils/ServerUtil.kt
+++ app/src/main/java/com/h2square/colosseum/utils/ServerUtil.kt
@@ -1,4 +1,11 @@
 package com.h2square.colosseum.utils
 
 class ServerUtil {
+    companion object{
+        val BASE_URL = "http://3.37.226.131"
+
+        fun postRequestLogin(){
+            
+        }
+    }
 }
