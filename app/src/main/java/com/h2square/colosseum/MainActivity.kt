f895478678d16980d9611c169d082902c77d234c
jungbin-lee <hihihi486@naver.com>
2023-05-22 20:47:27 +0900

    add fun login

--- app/src/main/java/com/h2square/colosseum/MainActivity.kt
+++ app/src/main/java/com/h2square/colosseum/MainActivity.kt
@@ -2,6 +2,10 @@ package com.h2square.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.h2square.colosseum.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity :BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,33 @@ class MainActivity :BaseActivity() {
     }
 
     override fun setupEvents() {
+        loginBtn.setOnClickListener{
+            val inputEmail =emailEdt.text.toString()
+            val inputPassword =passwordEdt.text.toString()
+
+
+            //(APIRequest)
+    ServerUtil.postRequestLogin(inputEmail,inputPassword, object :ServerUtil.Companion.JsonResponseHandler{
+        override fun onResponse(jsonObj: JSONObject) {
+            val code =jsonObj.getInt("code")
+            if(code==200){
+                //로그인 성공
+
+            }else{
+                //로그인 실패
+                val message = jsonObj.getString("message")
+                runOnUiThread{
+                    Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+    })
+
+
+
+        }
 
     }
 
--- app/src/main/java/com/h2square/colosseum/utils/ServerUtil.kt
+++ app/src/main/java/com/h2square/colosseum/utils/ServerUtil.kt
@@ -1,10 +1,50 @@
 package com.h2square.colosseum.utils
 
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
 class ServerUtil {
     companion object{
+
+        interface  JsonResponseHandler{
+            fun  onResponse(JsonObj:JSONObject)
+        }
         val BASE_URL = "http://3.37.226.131"
 
-        fun postRequestLogin(){
+        fun postRequestLogin(email: String, password: String,handler: JsonResponseHandler?){
+            //POST /users okHttp
+            val urlString= "${BASE_URL}/user"
+            val formData= FormBody.Builder()
+                .add("email",email)
+                .add("password",password)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+
+            val client =OkHttpClient()
+            client.newCall(request).enqueue(object :Callback{
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+
+                    val bodyString= response.body!!.string()
+                    val jsonObj= JSONObject(bodyString)
+                    Log.d("응답 본문",jsonObj.toString())
+
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            })
+
             
         }
     }
